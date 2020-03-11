ef6360607493085e9730f84aacd5076fe72dcb77
vikax <val.kucherbaev@gmail.com>
2020-03-11 09:16:17 +0500

    mody qr/scan - start lst camera

--- qr/scan/app.js
+++ qr/scan/app.js
@@ -10,13 +10,14 @@ var app = new Vue({
     var self = this;
     self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
     self.scanner.addListener('scan', function (content, image) {
+		
       self.scans.unshift({ date: +(Date.now()), content: content });
     });
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
       if (cameras.length > 0) {
-        self.activeCameraId = cameras[0].id;
-        self.scanner.start(cameras[0]);
+        self.activeCameraId = cameras[cameras.length-1].id;
+        self.scanner.start(cameras[cameras.length-1]);
       } else {
         console.error('No cameras found.');
       }
