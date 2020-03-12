8940ddbaf580f573300c6d584b472d3bc978aa41
vikax <val.kucherbaev@gmail.com>
2020-03-12 13:58:11 +0500

    mody010 qr/scan - test AbsoluteOrientationSensor

--- qr/scan/app.js
+++ qr/scan/app.js
@@ -38,6 +38,7 @@ alert("content:"+ ff.join("&\n<br>"));
   }
 });
 
+/*
 
 //alert(" window.addEventListener('devicelight'");
 //window.addEventListener('devicelight', function(event) {
@@ -84,4 +85,29 @@ if ("ProximitySensor" in window) {
 if (window.AmbientLightSensor) {
    alert("window.AmbientLightSensor");
 }
+
+
+
+*/
+
+var scene = new THREE.Scene();
+
+let torusGeometry = new THREE.TorusGeometry(7, 1.6, 4, 3, 6.3);
+let material = new THREE.MeshBasicMaterial({ color: 0x0071C5 });
+let torus = new THREE.Mesh(torusGeometry, material);
+scene.add(torus);
+
+// Update mesh rotation using quaternion.
+const sensorAbs = new AbsoluteOrientationSensor();
+sensorAbs.onreading = () => torus.quaternion.fromArray(sensorAbs.quaternion);
+sensorAbs.start();
+
+// Update mesh rotation using rotation matrix.
+const sensorRel = new RelativeOrientationSensor();
+let rotationMatrix = new Float32Array(16);
+sensor_rel.onreading = () => {
+    sensorRel.populateMatrix(rotationMatrix);
+    torus.matrix.fromArray(rotationMatrix);
+}
+sensorRel.start();
 
