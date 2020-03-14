cc760f2f1411330af9da930b2e21cbf3802fc8bf
vikax <val.kucherbaev@gmail.com>
2020-03-14 14:59:18 +0500

    mody014 qr/scan - unicode ciril

--- qr/scan/app.js
+++ qr/scan/app.js
@@ -10,10 +10,102 @@ var app = new Vue({
     var self = this;
     self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
     self.scanner.addListener('scan', function (content, image) {
-	let ff =  content.split("&");
-alert("content:"+ ff.join("&\n<br>"));
 
-      self.scans.unshift({ date: +(Date.now()), content: ff.join("&\n<br>") });
+function convertToHex(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        hex += ''+str.charCodeAt(i).toString(16)+" ";
+    }
+    return hex;
+}
+
+function DoTo4(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        let cc=str.charCodeAt(i).toString(16);
+
+        hex += ''+str.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
+	
+	function utf8_to_str(a) {
+		let s = ''; 
+		let last_h=0;
+		for(let i=0 ; i<a.length; i++) { 
+			let h = a.charCodeAt(i); 
+			if (( h == 0xd0 ) || (h == 0xd1) || (h == 0xc2))  {
+				if (h == 0xd0) last_h = 0xd0;
+				if (h == 0xd1) last_h = 0xd1;
+				if (h == 0xc2) last_h = 0xc2;
+			} else {	
+				if ((last_h == 0xd0 ) || (last_h == 0xd1 ) || (last_h == 0xc2 )) {
+					if (last_h == 0xd0 )  s += String.fromCharCode(0x400 + (h & 0x3f));
+					if (last_h == 0xd1 )  s += String.fromCharCode(0x440 + (h & 0x3f));
+					if (last_h == 0xc2 )  s += String.fromCharCode(0x00 + (h));
+				} else {
+					s += a[i];
+				}
+				last_h = h;
+			}
+			// alert("h="+h);
+		} 
+
+
+//let s0="&#x41f&#x410&#x41e &#x411&#x410&#x428&#x418&#x041d&#x424&#x41e&#x420&#x41c&#x421&#x412&#x42f&#x417&#x42c";
+//alert(" s0="+s0);
+//document.getElementById("info_qr_scan").innerHTML = s0;
+//alert(" hex s0="+convertToHex(s0));
+
+
+//alert(" s="+s);
+//document.getElementById("info_qr_scan").innerHTML = s;
+		return s; 
+	} 
+
+
+function convertToHex(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        hex += ''+str.charCodeAt(i).toString(16)+" ";
+    }
+    return hex;
+}
+
+function DoTo4(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        let cc=str.charCodeAt(i).toString(16);
+
+        hex += ''+str.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
+
+//document.getElementById("info_qr_scan").innerHTML = content;
+
+        content =   utf8_to_str(content);
+	document.getElementById("info_qr_scan").innerHTML = content;
+//        content =   convertToHex(content);
+
+//alert(content);
+//alert("ПРИВЕТ");	
+//alert(convertToHex("ПРИВЕТ"));	 // 41f 420 418 412 415 422
+//alert((encodeURIComponent(content)));	
+//alert(decodeURIComponent(encodeURIComponent(content)));	
+
+	let ff1 =  content.split("&");
+	let ff2 =  content.split("|");
+	if (ff1.length >1 ) {
+		alert("content:"+ ff1.join("&\n<br>"));
+      		self.scans.unshift({ date: +(Date.now()), content: ff1.join("&\n<br>") });
+	}
+
+	if (ff2.length >1 ) {
+		alert("content:"+ ff2.join("|\n<br>"));
+      		self.scans.unshift({ date: +(Date.now()), content: ff2.join("|\n<br>") });
+	}
+
     });
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
