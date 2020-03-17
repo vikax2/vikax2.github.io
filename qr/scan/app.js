52756ac111c971f3193a8451868c12969182632c
vikax <val.kucherbaev@gmail.com>
2020-03-17 09:21:55 +0500

    mody020 qr/scan - win1251 ciril

--- qr/scan/app.js
+++ qr/scan/app.js
@@ -28,12 +28,44 @@ function DoTo4(str) {
     }
     return hex;
 }
+/////////////////////////////////////////////
 	
-	function utf8_to_str(a) {
+	function win1251_to_str(a) {
+
 		let s = ''; 
 		let last_h=0;
 		for(let i=0 ; i<a.length; i++) { 
 			let h = a.charCodeAt(i); 
+			 let n = h & 0xf0;
+			if ( n < 0xc0 )  {
+				if (( h == 0xA8) || ( h == 0xA8)) {
+					if (( h == 0xA8)) {s += String.fromCharCode(0x401);}
+					if (( h == 0xB8)) {s += String.fromCharCode(0x451);}
+			 
+				} else {
+					s += a[i] ;
+				}
+			}
+
+			if ( n == 0xc0 )  {s += String.fromCharCode(0x410 + (h & 0x0f)) ;}
+			if ( n == 0xd0 )  {s += String.fromCharCode(0x420 + (h & 0x0f)) ;}
+			if ( n == 0xe0 )  {s += String.fromCharCode(0x430 + (h & 0x0f)) ;}
+			if ( n == 0xf0 )  {s += String.fromCharCode(0x440 + (h & 0x0f)) ;}
+
+			// alert("h="+h);
+		} 
+
+		return s; 
+	}
+/////////////////////////////////////////////
+	
+	function utf8_to_str(a) {
+
+		let s = ''; 
+		let last_h=0;
+		for(let i=0 ; i<a.length; i++) { 
+			let h = a.charCodeAt(i); 
+			
 			if (( h == 0xd0 ) || (h == 0xd1) || (h == 0xc2))  {
 				if (h == 0xd0) last_h = 0xd0;
 				if (h == 0xd1) last_h = 0xd1;
@@ -51,16 +83,68 @@ function DoTo4(str) {
 			// alert("h="+h);
 		} 
 
-
-//let s0="&#x41f&#x410&#x41e &#x411&#x410&#x428&#x418&#x041d&#x424&#x41e&#x420&#x41c&#x421&#x412&#x42f&#x417&#x42c";
-//alert(" s0="+s0);
-//document.getElementById("info_qr_scan").innerHTML = s0;
-//alert(" hex s0="+convertToHex(s0));
-
-
-//alert(" s="+s);
-//document.getElementById("info_qr_scan").innerHTML = s;
 		return s; 
+	}
+/////////////////////////////////////////////
+	
+	function koi8_to_str(a) {
+
+		let s = ''; 
+		let last_h=0;
+		for(let i=0 ; i<a.length; i++) { 
+			let h = a.charCodeAt(i); 
+			
+			if (( h == 0xd0 ) || (h == 0xd1) || (h == 0xc2))  {
+				if (h == 0xd0) last_h = 0xd0;
+				if (h == 0xd1) last_h = 0xd1;
+				if (h == 0xc2) last_h = 0xc2;
+			} else {	
+				if ((last_h == 0xd0 ) || (last_h == 0xd1 ) || (last_h == 0xc2 )) {
+					if (last_h == 0xd0 )  s += String.fromCharCode(0x400 + (h & 0x3f));
+					if (last_h == 0xd1 )  s += String.fromCharCode(0x440 + (h & 0x3f));
+					if (last_h == 0xc2 )  s += String.fromCharCode(0x00 + (h));
+				} else {
+					s += a[i];
+				}
+				last_h = h;
+			}
+			// alert("h="+h);
+		} 
+
+		return s; 
+	}
+
+////////////////////////////////////////////
+	function decode(a) {
+		let s = ''; 
+		let last_h=0;
+		let ste=0;
+		let stv=0;
+		let stn=0;
+if ("ST000"  == content.substr(0,5) ) {
+                ste=1; // ST000 exist
+	if ("1"  == content.substr(5,1) ) {
+		stv=1;
+		let lastC = content.substr(6,1);
+		switch (lastC) {
+			case "1":  stn=1; break;   //  windows-1251
+			case "2":  stn=2; break;   //  utf-8
+			case "3":  stn=3; break;   //  koi8
+		}
+	}
+
+}
+
+		if (ste) {
+			if (stv == 1) {
+			   if (stn == 1) return  win1251_to_str(a);
+			   if (stn == 2) return  utf8_to_str(a);
+			   if (stn == 3) return  koi8_to_str(a);
+			} 
+		}
+
+
+		return a; 
 	} 
 
 
@@ -83,14 +167,17 @@ function DoTo4(str) {
 }
 
 //document.getElementById("info_qr_scan").innerHTML = content;
+//        contentHex =   convertToHex(content);
+//  alert("000 "+contentHex);
 
 //  alert("111 "+content);
 
-        content =   utf8_to_str(content);
+	
+        content =   decode(content);
 //  alert("222 "+content);
 
 //	document.getElementById("info_qr_scan").innerHTML = content;
-//        content =   convertToHex(content);
+//        contentHex =   convertToHex(content);
 
 //alert(content);
 //alert("ПРИВЕТ");	
@@ -100,6 +187,9 @@ function DoTo4(str) {
 
 	let ff1 =  content.split("&");
 	let ff2 =  content.split("|");
+
+
+
 	if (ff1.length >1 ) {
 		alert("content:"+ ff1.join("&\n<br>"));
 //      		self.scans.unshift({ date: +(Date.now()), content: ff1.join("&\n<br>") });
